9e8e9579c3ca1cf93212a5d2ee13c4c4d207ebc7
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-09-23 21:32:50 +0700

    test: try pool connection

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,15 @@
 use actix_web::{App, HttpServer, Result, get, web};
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
 
 #[get("/{id}")]
 async fn greet(path: web::Path<String>) -> Result<String> {
@@ -8,6 +19,7 @@ async fn greet(path: web::Path<String>) -> Result<String> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let pool = establish_connection();
     HttpServer::new(|| {
         App::new()
             .service(greet)
