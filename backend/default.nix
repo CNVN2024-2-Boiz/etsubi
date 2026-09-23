79b8655698e12de8b797af446b9b2295e37d69aa
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-09-23 21:31:55 +0700

    feat: add nix environment

---
+++ backend/default.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    # rustup
+    pkg-config
+    clang
+    llvmPackages.bintools
+    cmake
+    gnumake
+    perl
+  ];
+
+  buildInputs = with pkgs; [
+    postgresql
+    openssl
+    zlib
+    zstd
+  ];
+
+  shellHook = ''
+    export PKG_CONFIG_PATH="${pkgs.postgresql.lib}/lib/pkgconfig:$PKG_CONFIG_PATH"
+    export PQ_LIB_DIR="${pkgs.postgresql.lib}/lib"
+    export LD_LIBRARY_PATH="${pkgs.postgresql.lib}/lib:${pkgs.openssl.out}/lib:$LD_LIBRARY_PATH"
+    export RUSTFLAGS="-C link-arg=-fuse-ld=lld"
+  '';
+}
